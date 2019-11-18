17cf74bc37fc24cc9636b429e21f73f4d32239ca
Haozhi <34656389+haozhima95@users.noreply.github.com>
2019-11-18 14:20:08 +0100

    Create wdpa_point_splitbiome.js

---
+++ codes/wdpa_codes/wdpa_point_splitbiome.js
@@ -0,0 +1,24 @@
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+// This script is used for exporting wdpa points by biomes. 
+// All the sub-records will be sampled by environmental covariates.
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+
+for(var i = 1; i<15; i++){
+// Define biome region
+  var region = biome.filterMetadata('BIOME','equals',i);
+// clip the feature collection
+  var subtable = table.filterBounds(region);
+  print(i);
+  print(subtable.size());
+  // Explore the point size. 
+  // Export the tables.
+  Export.table.toAsset({
+    collection:subtable,
+    description:'wdpa_subpoints'+i,
+    assetId:'users/haozhima95/forest_pheno/wdpa_subpoint_'+i+'_20191118'
+  });
+  
+}
